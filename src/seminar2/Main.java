bab0bd4065bd6686a66bad3a388f910bce1a8ba0
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:33:01 +0500

    Update Main.java

--- src/seminar2/Main.java
+++ src/seminar2/Main.java
@@ -1,2 +1,24 @@
-package seminar2;public class Main {
+package seminar2;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setName("Борис");
+        /*human.setMakeOrder();
+        human.setTakeOrder();
+        human.isMakeOrder();
+        System.out.println(human.isMakeOrder());
+        human.isTakeOrder();
+        System.out.println(human.isTakeOrder());*/
+
+        Market market = new Market();
+        market.acceptToMarket(human);
+        market.takeQueue(human);
+        market.takeOrders();
+        market.update();
+        market.giveOrders();
+        market.releaseFromQueue(human);
+        market.releaseFromMarket(human);
+
+    }
 }
