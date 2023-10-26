4725117fe1d04843a49743b5d128d987c357ade8
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:38:37 +0500

    Update Teacher.java

--- src/seminar4/Teacher.java
+++ src/seminar4/Teacher.java
@@ -1,2 +1,43 @@
-package seminar4;public class Teacher {
+package seminar4;
+
+abstract class Teacher {
+    String name;
+    String patronymic;
+    String surname;
+
+    protected Teacher(String name, String patronymic, String surname) {
+        this.name = name;
+        this.patronymic = patronymic;
+        this.surname = surname;
+    }
+
+    public String getName() {return name;}
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher: " +
+                "name:'" + name + '\'' +
+                ", patronymic:'" + patronymic + '\'' +
+                ", surname='" + surname + '\'';
+    }
 }
