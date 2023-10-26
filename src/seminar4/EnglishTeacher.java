6a9076c1e471afb9fbba8f824339c9c716e2af64
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:37:35 +0500

    Update EnglishTeacher.java

--- src/seminar4/EnglishTeacher.java
+++ src/seminar4/EnglishTeacher.java
@@ -1,2 +1,15 @@
-package seminar4;public class EnglishTeacher {
+package seminar4;
+
+public class EnglishTeacher extends Teacher{
+    protected EnglishTeacher(String name, String patronymic, String surname) {
+        super(name, patronymic, surname);
+    }
+
+    @Override
+    public String toString() {
+        return  "EnglishTeacher: " +
+                "name:'" + name + '\'' +
+                ", patronymic:'" + patronymic + '\'' +
+                ", surname='" + surname + '\'';
+    }
 }
