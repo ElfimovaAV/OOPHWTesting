352d28277c86ecfc73c7ebbe1ab76e95f2ce7e76
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:33:36 +0500

    Update MarketBehaviour.java

--- src/seminar2/MarketBehaviour.java
+++ src/seminar2/MarketBehaviour.java
@@ -3,5 +3,5 @@ package seminar2;
 public interface MarketBehaviour {
     void acceptToMarket(Human humans); //приход покупателей
     void releaseFromMarket (Human humans); //уход покупателей
-    void update(); // обновшление магазина
+    void update(); // обновление магазина
 }
