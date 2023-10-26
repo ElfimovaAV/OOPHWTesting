846b3fba30c226c77e5cc2080294aba9b5e96f32
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:37:59 +0500

    Update Main.java

--- src/seminar4/Main.java
+++ src/seminar4/Main.java
@@ -1,2 +1,20 @@
-package seminar4;public class Main {
+package seminar4;
+
+
+public class Main {
+    public static void main(String[] args) {
+        TeachersGroup<Teacher> teachers = new TeachersGroup<>("teachers");
+        teachers.addTeacher(new MathTeacher("Петр", "Васильевич", "Кораблев"));
+        teachers.addTeacher(new MathTeacher("Лариса", "Андреевна", "Почалкина"));
+        teachers.addTeacher(new EnglishTeacher("Борис", "Федорович", "Земкин"));
+        teachers.addTeacher(new EnglishTeacher("Мария", "Кирилловна", "Пономарева"));
+
+
+        for(
+                int index = 0; index<teachers.count();index++)
+
+        {
+            System.out.println(teachers.get(index));
+        }
+    }
 }
