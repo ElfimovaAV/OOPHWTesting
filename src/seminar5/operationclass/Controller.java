f8b3645d471c3089a38b70fa92cd1149d8692ac7
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:30:18 +0500

    Update Controller.java

--- src/seminar5/operationclass/Controller.java
+++ src/seminar5/operationclass/Controller.java
@@ -1,18 +1,33 @@
-package seminar5;
+package seminar5.operationclass;
 
 import seminar5.baseclass.Student;
 import seminar5.baseclass.StudyGroup;
+import seminar5.baseclass.Teacher;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
-    private StudyGroupService studyGroupService;
+    private static final StudyGroupService service = new StudyGroupService();
 
-    public Controller(StudyGroupService studyGroupService) {
-        this.studyGroupService = studyGroupService;
+    public static StudyGroup createGroup(){
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Ivan", "Vladimirovich", "Bunin"));
+        students.add(new Student("Petr", "Kirillovich", "Stolypin"));
+        students.add(new Student("Nikolay", "Grigorievich", "Berezhnoy"));
+        students.add(new Student("Evgeniya", "Alekseevna", "Lisichkina"));
+        students.add(new Student("Vera", "Antonovna", "Ivanova"));
+
+        return new StudyGroup(1, "mat_meh", new Teacher("Pavel", "Borisovich","Zagorin"), students);
     }
 
-    public StudyGroup createStudyGroup (Teacher teacher, List<Student> students) {
-        return new StudyGroup(teacher, students);
+    public static StudyGroup readGroup(String fileName){
+        return service.loadGroupFromFile(fileName);
     }
+
+    public static void saveGroup(String fileName, StudyGroup group){
+        service.saveGroupToFile(fileName, group);
+    }
+
 }
