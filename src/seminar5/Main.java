ea78418950c1c4d7918ae6a572a5c5a84b096fb6
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:31:06 +0500

    Update Main.java

--- src/seminar5/Main.java
+++ src/seminar5/Main.java
@@ -1,4 +1,17 @@
 package seminar5;
 
+import seminar5.baseclass.StudyGroup;
+import seminar5.operationclass.Controller;
+
+
 public class Main {
+    public static void main(String[] args) {
+
+        StudyGroup group = Controller.createGroup();
+
+        Controller.saveGroup("group.txt", group);
+
+        StudyGroup newGroup = Controller.readGroup("group.txt");
+
+    }
 }
