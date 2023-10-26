d403cb99cfb2bc614e34130e3cb7250e7bc97605
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:33:52 +0500

    Update QueueBehaviour.java

--- src/seminar2/QueueBehaviour.java
+++ src/seminar2/QueueBehaviour.java
@@ -1,2 +1,8 @@
-package seminar2;public interface QueueBehaviour {
+package seminar2;
+
+public interface QueueBehaviour {
+    void takeQueue(Human humans); // помещение в очередь
+    void takeOrders(); // принятие заказа
+    void giveOrders(); // отдача заказа
+    void releaseFromQueue(Human humans); // освобождение из очереди
 }
