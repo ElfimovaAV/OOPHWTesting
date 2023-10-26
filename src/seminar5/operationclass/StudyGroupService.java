406bd44fcc7272cdb91baa0616bcece95ab03567
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:30:48 +0500

    Update StudyGroupService.java

--- src/seminar5/operationclass/StudyGroupService.java
+++ src/seminar5/operationclass/StudyGroupService.java
@@ -1,13 +1,45 @@
-package seminar5;
+package seminar5.operationclass;
 
-import seminar5.baseclass.Student;
 import seminar5.baseclass.StudyGroup;
 
-import java.util.List;
+import java.io.*;
+import java.util.logging.Logger;
 
 public class StudyGroupService {
 
-    public StudyGroup createStudyGroup (Teacher teacher, List<Student> students) {
-        return new StudyGroup(teacher, students);
+    public StudyGroupService() {
     }
+    public String saveGroupToFile(String fileName, StudyGroup group){
+        String result = "Saved to a file";
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+            outputStream.writeObject(group);
+            outputStream.close();
+        } catch (IOException e) {
+            result = e.getMessage();
+        }
+        Logger logger = Logger.getAnonymousLogger();
+        logger.info(result);
+
+        return result;
+    }
+
+    public StudyGroup loadGroupFromFile(String fileName){
+        StudyGroup group;
+        try{
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
+            group = (StudyGroup) inputStream.readObject();
+            inputStream.close();
+        } catch (IOException | ClassNotFoundException e) {
+            Logger logger = Logger.getAnonymousLogger();
+            logger.info(e.getMessage());
+            group = null;
+        }
+
+        return group;
+    }
+
 }
