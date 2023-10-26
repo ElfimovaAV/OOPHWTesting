2bf37315f694b95e8867d7c7d484dbc0b173f5c5
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:38:17 +0500

    Update MathTeacher.java

--- src/seminar4/MathTeacher.java
+++ src/seminar4/MathTeacher.java
@@ -1,2 +1,15 @@
-package seminar4;public class MathTeacher {
+ackage seminar4;
+
+public class MathTeacher extends Teacher{
+    protected MathTeacher(String name, String patronymic, String surname) {
+        super(name, patronymic, surname);
+    }
+
+    @Override
+    public String toString() {
+        return  "MathTeacher: " +
+                "name:'" + name + '\'' +
+                ", patronymic:'" + patronymic + '\'' +
+                ", surname='" + surname + '\'';
+    }
 }
