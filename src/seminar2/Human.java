0100b7b01e6fad5fa7a166fc52f78678ff1b757d
ElfimovaAV <120068039+ElfimovaAV@users.noreply.github.com>
2023-10-26 20:32:44 +0500

    Update Human.java

--- src/seminar2/Human.java
+++ src/seminar2/Human.java
@@ -1,2 +1,33 @@
-package seminar2;public class Human {
+package seminar2;
+
+public class Human extends Actor implements ActorBehaviour {
+    @Override
+    String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setMakeOrder() {
+        isMakeOrder = true;
+        System.out.println("Заказ принят");
+    }
+
+    @Override
+    public void setTakeOrder() {
+        isTakeOrder = true;
+        System.out.println("Заказ выдан");
+    }
+
+    @Override
+    public boolean isMakeOrder() {
+        return isMakeOrder;
+    }
+
+    @Override
+    public boolean isTakeOrder() {
+        return isTakeOrder;
+    }
 }
